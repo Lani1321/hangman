e653b644677ff4328c5babb705e8706ffa2b3c4d
Lani1321 <lani.berry13@gmail.com>
2016-11-30 15:44:15 -0500

    add methods for reading and updating the current game from/to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_game
+
+  # Loads and keeps the current game
+  def current_game
+    @current_game ||= load_current_game
+  end
+
+  # Sets both the @current_game variable & the session's 
+  # serialized current game with the given game
+  def set_current_game(game)
+    @current_game = game
+    session[:serialized_current_game] = game.present? ? game.to_json : nil
+  end
+
+  # Serialize the current game to session
+  def update_current_game
+    set_current_game @current_game
+  end
+
+  protected
+
+  def load_current_game
+    Game.new.from_json(session[:serialized_current_game]) if session[:serialized_current_game].present?
+  end
+
 end
