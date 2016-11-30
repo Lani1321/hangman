8fed516c8e2c47d12112c0110a0bb7f61ac93ce2
Lani1321 <lani.berry13@gmail.com>
2016-11-30 12:19:22 -0500

    add controller to create, show, update and cancel a game

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'games/new'
+
+  get 'games/show'
+
+  get 'games/update'
+
+  get 'games/destroy'
+
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
