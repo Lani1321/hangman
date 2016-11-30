91efb88f961fdb0e2a7865bde925a1245652cf26
Lani1321 <lani.berry13@gmail.com>
2016-11-30 12:21:47 -0500

    define root path with HomeController index action, and defined game resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'games/new'
 
-  get 'games/show'
+  root :to => 'home#index'
 
-  get 'games/update'
-
-  get 'games/destroy'
-
-  get 'home/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :game, :only => [:new, :show, :update, :destroy]
+  
 end
