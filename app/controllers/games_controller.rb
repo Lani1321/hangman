1a7942a516fbc3b054e8144be5f5e5c36b37a497
Lani1321 <lani.berry13@gmail.com>
2016-12-01 15:16:52 -0500

    add update action so letter guesses are added after each turn, add delete action to cancel a current game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   def new
-    session[:current_game] = Game.new
+    set_current_game Game.new
     redirect_to game_path
   end
 
@@ -8,8 +8,16 @@ class GamesController < ApplicationController
   end
 
   def update
+    current_game.select! params[:letter]
+    update_current_game
+  rescue Game::GameOverError
+    flash[:alert] = 'This game is finished...'
+  ensure
+    redirect_to game_path
   end
 
   def destroy
+    set_current_game nil
+    redirect_to root_path
   end
 end
